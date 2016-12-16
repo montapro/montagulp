f8893706653825d0b180ce569a4acaa175b34285
montapro <Mkmk#0296->
2016-12-16 15:45:44 +0100

    only scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,28 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
+
+
+var srcFiles = {
+    styles: './scss/*.scss'
+};
+
+var distFolders = {
+    styles: './css/'
+};
+
+
+
 gulp.task('styles', function () {
-    return gulp.src('./scss/*.scss')
+    return gulp.src(srcFiles.styles)
         .pipe(sass())
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest(distFolders.styles));
 });
 
-// gulp.task('scripts', function() {
-//     gulp.src(source_files.vendor_scripts)
-//         .pipe(debug({title: "vendor-scripts"}))
-//         .pipe(vendor('vendor.js'))
-//         .pipe(gulp.dest(dist_folders.scripts));
-//
-//     return gulp.src(source_files.scripts)
-//         .pipe(debug({title: "scripts"}))
-//         .pipe(ignore('_*.js'))
-//         .pipe(include())
-//         .pipe(plumber())
-//         .pipe(gulp.dest(dist_folders.scripts));
-// });
+
 
 gulp.task('watch', function () {
-    gulp.watch('./scss/*scss', ['styles']);
+    gulp.watch(srcFiles.styles, ['styles']);
 });
 
 gulp.task('default', ['styles']);
