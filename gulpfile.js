09e612deea25d065503dbc31b4ffa039ae331c89
montapro <Mkmk#0296->
2016-12-19 08:48:15 +0100

    v1

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,37 @@
+/**
+ * DEPENDENCIES
+ */
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-
-
-
-var srcFiles = {
-    styles: './scss/*.scss'
-};
-
-var distFolders = {
-    styles: './css/'
-};
+var browserSync = require('browser-sync');
+var autoPrefixer = require('gulp-autoprefixer');
 
 
 
 gulp.task('styles', function () {
-    return gulp.src(srcFiles.styles)
+    gulp.src('./src/scss/main.scss')
         .pipe(sass())
-        .pipe(gulp.dest(distFolders.styles));
+        .pipe(autoPrefixer({
+            browsers: ['> 0%'],
+            cascade: true
+        }))
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(browserSync.reload({stream: true}));
 });
 
 
 
-gulp.task('watch', function () {
-    gulp.watch(srcFiles.styles, ['styles']);
+gulp.task('serve', function () {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+    gulp.watch('./src/scss/*.scss', ['styles']);
+    gulp.watch('./**/*.html').on('change', browserSync.reload);
+    gulp.watch('./**/*.php').on('change', browserSync.reload);
 });
 
-gulp.task('default', ['styles']);
+
+
+gulp.task('default', ['styles', 'serve']);
