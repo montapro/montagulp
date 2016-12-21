f89e77879e859d40c6715389a8ea0c194e1a4814
montapro <Mkmk#0296->
2016-12-21 08:51:24 +0100

    sass sourcemaps, sass error logs

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,22 @@
  */
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-const browserSync = require('browser-sync');
 const autoPrefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
 const babel = require('gulp-babel');
+const browserSync = require('browser-sync');
 
 
 
 gulp.task('styles', function () {
     gulp.src('./src/scss/main.scss')
-        .pipe(sass())
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
         .pipe(autoPrefixer({
             browsers: ['> 0%'],
             cascade: true
         }))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('./dist/css'))
         .pipe(browserSync.reload({stream: true}));
 });
@@ -33,12 +36,12 @@ gulp.task('scripts', function() {
 
 
 gulp.task('serve', function () {
-    // browserSync.init({
-    //     server: {
-    //         // baseDir: './../../',
-    //         proxy: 'http://ambicon'
-    //     }
-    // });
+    browserSync.init({
+        server: {
+            baseDir: './',
+            proxy: 'http://ambicon:8888'
+        }
+    });
     gulp.watch('./src/scss/*.scss', ['styles']);
     gulp.watch('./src/js/*.js', ['scripts']);
     gulp.watch('./**/*.html').on('change', browserSync.reload);
